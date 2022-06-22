3ee3185835bb30eb195dd61ff7f7f836c6a5c92b
nubilsa <sandrinenubia@gmail.com>
2022-06-22 01:09:53 -0700

    debug

---
+++ 01-Day1/3-debugging.py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manipulation")
+print("String concatenation is done with the" + " sign")
+print("Me" + " " + "gusta")
+print("new lines can be created with \nThe backslash an n like this one \nhahhaahahahahah")
