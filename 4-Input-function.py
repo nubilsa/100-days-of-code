15e8a37949c5e4c72a2539f68510476baa523bb2
nubilsa <sandrinenubia@gmail.com>
2022-06-22 01:29:30 -0700

    4

--- 4-Input-function.py
+++ 4-Input-function.py
@@ -1,2 +1,4 @@
-input("Hey what's your name? ")
-print("welcome " + input("Hey what's your name? ") + input("\nWhat brings you here today?"))
+#input("Hey what's your name? ")
+#print("welcome " + input("Hey what's your name? ") + input("\nWhat brings you here today?"))
+print("welcome " + input("Hey what's your name? "))
+print("Our reps are currently busy assisting other customer please wait a moment" + input("in a few words tell us what you need"))
