3975dee87fc76a741747496e448f7ce38987cb34
nubilsa <sandrinenubia@gmail.com>
2022-06-22 12:46:34 -0700

    congrats

---
+++ 8-variable-exo.py
@@ -0,0 +1,15 @@
+# not chhange
+a = input("Please enter a number ")
+b = input("please enter a letter ")
+# switch
+# a = print(b)
+# b = print(a)
+# print
+# print("a = " + b)
+# print("b = " + a)
+
+c = a
+
+
+a = print(b)
+b = print(c)
